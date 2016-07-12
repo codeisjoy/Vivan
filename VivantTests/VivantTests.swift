f076b45e26c88537993581dfa82043d4115a1ba5
Emad A <emad310@gmail.com>
2016-07-13 09:23:02 +1000

    Add network test cases

--- VivantTests/VivantTests.swift
+++ VivantTests/VivantTests.swift
@@ -11,25 +11,36 @@ import XCTest
 
 class VivantTests: XCTestCase {
     
+    var network: NetworkCenter?
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        network = NetworkCenter.defaultCenter
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testGettingPosts() {
+        let expectation = expectationWithDescription("getting_posts")
+        _ = network?.getPosts { posts, error in
+            XCTAssertNil(error, error!.message)
+            XCTAssertNotNil(posts)
+            expectation.fulfill()
+        }
+        waitForExpectationsWithTimeout(10) { [weak self] error in
+            guard let error = error else { return }
+            self?.recordFailureWithDescription(error.debugDescription, inFile: #file, atLine: #line, expected: false)
+        }
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testDownloadingAPhoto() {
+        let expectation = expectationWithDescription("downloading_photo")
+        _ = network?.getPhoto("one.jpg") { image, error in
+            XCTAssertNil(error, error!.message)
+            XCTAssertNotNil(image)
+            expectation.fulfill()
+        }
+        waitForExpectationsWithTimeout(10) { [weak self] error in
+            guard let error = error else { return }
+            self?.recordFailureWithDescription(error.debugDescription, inFile: #file, atLine: #line, expected: false)
         }
     }
     
